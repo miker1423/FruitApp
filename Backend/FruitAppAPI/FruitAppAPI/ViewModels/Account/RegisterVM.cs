60cce9c21604e6b1c12a58e18d3fb5e95e7020f2
Miguel Pérez García <miguelpg_95@hotmail.com>
2017-10-16 09:38:11 -0500

    agregando data notation

--- Backend/FruitAppAPI/FruitAppAPI/ViewModels/Account/RegisterVM.cs
+++ Backend/FruitAppAPI/FruitAppAPI/ViewModels/Account/RegisterVM.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FruitAppAPI.ViewModels.Account
 {
     public class RegisterVM
     {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
         public string Nickname { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
     }
 }
